2662bd4a13916f32bb0b788d10378548c0ecd18e
Vaibhav Mittal <vaibhavmittal.26@gmail.com>
2023-09-09 14:22:02 +0530

    added hash function to load

--- src/functions/http-trigger-node-function.ts
+++ src/functions/http-trigger-node-function.ts
@@ -1,15 +1,27 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { availableParallelism } from "node:os";
+import { pbkdf2 } from "node:crypto";
+
+const start = Date.now();
 
 export const  httpTrigger = async(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit>=> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    console.log(`availableParallelism :${availableParallelism}`);
+    context.log(`Available parallelism ${availableParallelism}`)
+
+    doHash(context)
+    doHash(context);
 
     const name = request.query.get('name') || await request.text() || 'world';
 
-    return { body: `Hello, ${name}! with parallelism as ${availableParallelism}` };
+    return { body: `Hello, ${name}!` };
 };
+
+const doHash = (context:InvocationContext)=>{
+    pbkdf2('a','b',100000,512,'sha512',()=>{
+        context.log('Hash:', Date.now()-start);
+    })
+}
 
 app.http('http-trigger-node-function', {
     methods: ['GET', 'POST'],
